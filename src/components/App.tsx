3474c1b189edeccb451bc22a8ae976861718ca6a
Antti Leppä <antti.leppa@metatavu.fi>
2019-10-15 17:54:44 +0300

    Added proof of concept posts listsing into Welcome page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
+import Api from "../wpapi";
 import "../styles/styles.scss";
+
+Api.configure("http://localhost:1234/wp-json/");
+
+/**
+ * Interface representing component properties
+ */
+interface Props {
+}
+
+/**
+ * Interface representing component state
+ */
+interface State {
+}
 
 /**
  * App component
  */
-class App extends React.Component {
+class App extends React.Component<Props, State> {
 
   /**
    * Component render method
--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,9 +1,47 @@
 import * as React from "react";
+import Api from "../wpapi";
+import { Post } from "../wpapi";
+
+/**
+ * Interface representing component properties
+ */
+interface Props {
+}
+
+/**
+ * Interface representing component state
+ */
+interface State {
+  posts: Post[]
+}
 
 /**
  * WelcomePage component
  */
-class WelcomePage extends React.Component {
+class WelcomePage extends React.Component<Props, State> {
+
+  /**
+   * Constructor
+   *
+   * @param props component properties
+   */
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  /**
+   * Component did mount life-cycle handler
+   */
+  public componentDidMount = async () => {
+    const service = Api.getDefaultService("TOKEN");
+
+    this.setState({
+      posts: await service.getWpV2Posts()
+    });
+  }
 
   /**
    * Component render method
@@ -11,7 +49,27 @@ class WelcomePage extends React.Component {
   public render() {
     return (
       <div>
-        <p>Heippaloinen!</p>
+        { this.renderPosts() }
+      </div>
+    );
+  }
+
+  /**
+   * Renders list of posts
+   */
+  private renderPosts = () => {
+    return (
+      <div>
+        {
+          this.state.posts.map((post) => {
+            return (
+              <div key={ post.id }> 
+                <h1> { post.title ? post.title.rendered : "" } </h1>
+                <p dangerouslySetInnerHTML={ {__html: post.content ? post.content.rendered || "" : "" }} />
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
