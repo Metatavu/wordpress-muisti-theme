076d3e6c6318263fb177aca8f649599913674bb3
Antti Leppä <antti.leppa@metatavu.fi>
2019-10-21 12:26:00 +0300

    Removed wpapi

--- src/wpapi/model/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User;
-(function (User) {
-    User.LocaleEnum = {
-        US: 'en_US'
-    };
-})(User = exports.User || (exports.User = {}));
