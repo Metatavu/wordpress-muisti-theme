44f50561ba72b8343ceb2facdf644d8b6780852a
Tero Lamminpaa <tero@pop-os.localdomain>
2020-01-21 14:16:18 +0200

    Favicon added

--- inc/template-head-generic.php
+++ inc/template-head-generic.php
@@ -2,6 +2,7 @@
 <meta http-equiv="Content-language" content="${pageContext.request.locale.language}">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.metatavu.io/fonts/tt_norms/stylesheet.css" />
+<link rel="icon" type="image/png" href="<?php bloginfo('template_url') ?>/inc/favicon.png" sizes="512x512" />
 <title>Muisti</title>
 <script>
   window.THEME_PATH = "<?php bloginfo('template_url');?>";
