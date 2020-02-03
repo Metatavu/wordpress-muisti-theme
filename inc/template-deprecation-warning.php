8adb87bed407481e3e6ecc218795bca1e1a49c22
Tero Lamminpaa <tero@pop-os.localdomain>
2020-02-03 12:58:10 +0200

    Deprecation warning will be displayed when browsers ie11 and below are detected

---
+++ inc/template-deprecation-warning.php
@@ -0,0 +1,18 @@
+<div class="wrapper">
+  <p>
+    We detected that you are using deprecated legacy browser Internet Explorer.
+    Please switch to supported Microsoft Edge browser to continue using this site.
+  </p>
+</div>
+<style>
+  .wrapper {
+    width: 60%;
+    padding: 1rem;
+    margin-top: 2%;
+    margin-left: 20%;
+    margin-right: 20%;
+    text-align: "center";
+    background-color: "#e3e3e3";
+    font-family: "Arial, Helvetica, sans-serif";
+  }
+</style>
