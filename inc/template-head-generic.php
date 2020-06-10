9ff1e64e1446c3ebb44da1a26a18f1ddc7bfa508
Tero Lamminpaa <tero@pop-os.localdomain>
2020-06-10 13:21:24 +0300

    Featured image path is now og:image tag content

--- inc/template-head-generic.php
+++ inc/template-head-generic.php
@@ -1,6 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="Content-language" content="${pageContext.request.locale.language}">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'post-thumbnail' ) ?>">
 <link rel="stylesheet" href="https://cdn.metatavu.io/fonts/tt_norms/stylesheet.css" />
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/inc/loader.css">
 <link rel="icon" type="image/png" href="<?php bloginfo('template_url') ?>/inc/favicon.png" sizes="512x512" />
